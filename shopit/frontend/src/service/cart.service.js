79d6d13a6454847e0ec5869807762d358435ba27
Shani Anconina <shanianconina310@gmail.com>
2023-02-05 21:39:31 +0200

    add cart service

---
+++ shopit/frontend/src/service/cart.service.js
@@ -0,0 +1,21 @@
+import { storageService } from "./async-storage.service"
+import { httpService } from "./http.service"
+import { utilService } from "./util.service"
+
+const STORAGE_KEY = "cart_DB"
+
+export const cartService = {
+  query,
+  save,
+  remove,
+}
+
+function query() {
+  return sessionStorage.getItem(STORAGE_KEY)
+}
+
+function save(product) {
+  const products = query()
+  products.push(product)
+  return sessionStorage.setItem(STORAGE_KEY, products)
+}
