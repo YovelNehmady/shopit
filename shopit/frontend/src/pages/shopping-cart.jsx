5c49cd8524443c3bef7a5285b23b37697f396610
YovelNehmady <yovelnehmady1998@gmail.com>
2023-02-05 22:06:02 +0200

    done job interview demo2 with Shani

--- shopit/frontend/src/pages/shopping-cart.jsx
+++ shopit/frontend/src/pages/shopping-cart.jsx
@@ -1,5 +1,5 @@
-export function ShoppingCart (){
+export function ShoppingCart() {
     return <section className="shopping-cart">
-        Holla from cart
+
     </section>
 }
--- shopit/frontend/src/service/cart.service.js
+++ shopit/frontend/src/service/cart.service.js
@@ -1,22 +1,18 @@
-import { storageService } from "./async-storage.service"
-import { httpService } from "./http.service"
-import { utilService } from "./util.service"
-
 const STORAGE_KEY = "cart_DB"
 
 export const cartService = {
   query,
-  addToCart,
+  addToCart
 }
 
 function query() {
-  let cart = sessionStorage.getItem(STORAGE_KEY)
+  let cart = JSON.parse(sessionStorage.getItem(STORAGE_KEY))
   if (!cart || !cart.length) cart = []
   return cart
 }
 
 function addToCart(product) {
-  const products = query()
+  let products = query()
   products.push(product)
-  return sessionStorage.setItem(STORAGE_KEY, products)
+  return sessionStorage.setItem(STORAGE_KEY, JSON.stringify(products))
 }
